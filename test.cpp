b14736086c6ae19e44dcb4474b4467db9f92ffd8
unchunks <117646402+unchunks@users.noreply.github.com>
2023-09-27 22:03:31 +0900

    save 9-27

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@
 
 AreaDivide areaDivide;
 Player player(0, 0, 0, 0, 0);
-std::vector<Enemy> enemies = std::vector<Enemy>(NUM_ENEMY, Enemy(0, 0, 0, 0, 0));
+std::vector<Enemy> enemies = std::vector<Enemy>(NUM_ENEMY, Enemy(SLIME));
 CELL_TYPE floorTYPE[FLOOR_H][FLOOR_W];
 
 glm::vec2 getRandomPos(int roomCount)
@@ -24,61 +24,62 @@ void out()
 {
     bool charDrawn = false;
 	for(int y = 0; y < FLOOR_H; y++){
-		for(int x = 0; x < FLOOR_W; x++){
-            switch(data[y][x].parent)
-            {
-                case LEFT: std::cout << "←"; break;
-                case RIGHT: std::cout << "→"; break;
-                case UP: std::cout << "↑"; break;
-                case DOWN: std::cout << "↓"; break;
-                default: std::cout << " ";
-            }
-        }
+		// for(int x = 0; x < FLOOR_W; x++){
+        //     switch(data[y][x].parent)
+        //     {
+        //         case LEFT: std::cout << "←"; break;
+        //         case RIGHT: std::cout << "→"; break;
+        //         case UP: std::cout << "↑"; break;
+        //         case DOWN: std::cout << "↓"; break;
+        //         default: std::cout << " ";
+        //     }
+        // }
 
-        std::cout << " | ";
+        // std::cout << " | ";
+
+        // for(int x = 0; x < FLOOR_W; x++){
+        //     std::cout << data[y][x].status;
+        // }
+
+        // std::cout << " | ";
 
         for(int x = 0; x < FLOOR_W; x++){
-            std::cout << data[y][x].status;
-        }
-
-        std::cout << " | ";
-
-        for(int x = 0; x < FLOOR_W; x++){
-            charDrawn = false;
-            // SDLでは後ろから重ねて描画する
-            if((player.getX() == x) && (player.getY() == y)) {
-                std::cout << "@";
-                charDrawn = true;
-            }
-            for(auto e : enemies) {
-                if((e.getX() == x) && (e.getY() == y)) {
-                    std::cout << "E";
-                    charDrawn = true;
-                }
-            }
-            if(charDrawn)
-                continue;
-            switch(floorTYPE[y][x]) {
-                case NONE:              std::cout << " "; break;
-                case PILLAR:            std::cout << "W"; break;
-                case WALL_LEFT:         std::cout << "W"; break;
-                case WALL_RIGHT:        std::cout << "W"; break;
-                case WALL_TOP:          std::cout << "W"; break;
-                case WALL_BOTTOM:       std::cout << "W"; break;
-                case WALL_LEFT_TOP:     std::cout << "W"; break;
-                case WALL_LEFT_BOTTOM:  std::cout << "W"; break;
-                case WALL_RIGHT_TOP:    std::cout << "W"; break;
-                case WALL_RIGHT_BOTTOM: std::cout << "W"; break;
-                case WALL_SIDE_LR:      std::cout << "W"; break;
-                case WALL_SIDE_TB:      std::cout << "W"; break;
-                case WALL_END_LEFT:     std::cout << "W"; break;
-                case WALL_END_RIGHT:    std::cout << "W"; break;
-                case WALL_END_TOP:      std::cout << "W"; break;
-                case WALL_END_BOTTOM:   std::cout << "W"; break;
-                case WALL_ALL:          std::cout << "W"; break;
-                case FLOOR: case AISLE: std::cout << " "; break;
-                case STEP:              std::cout << "S"; break;
-            }
+            std::cout << floorTYPE[y][x];
+            // charDrawn = false;
+            // // SDLでは後ろから重ねて描画する
+            // if((player.getX() == x) && (player.getY() == y)) {
+            //     std::cout << "@";
+            //     charDrawn = true;
+            // }
+            // for(auto e : enemies) {
+            //     if((e.getX() == x) && (e.getY() == y)) {
+            //         std::cout << "E";
+            //         charDrawn = true;
+            //     }
+            // }
+            // if(charDrawn)
+            //     continue;
+            // switch(floorTYPE[y][x]) {
+            //     case NONE:              std::cout << " "; break;
+            //     case PILLAR:            std::cout << "W"; break;
+            //     case WALL_LEFT:         std::cout << "W"; break;
+            //     case WALL_RIGHT:        std::cout << "W"; break;
+            //     case WALL_TOP:          std::cout << "W"; break;
+            //     case WALL_BOTTOM:       std::cout << "W"; break;
+            //     case WALL_LEFT_TOP:     std::cout << "W"; break;
+            //     case WALL_LEFT_BOTTOM:  std::cout << "W"; break;
+            //     case WALL_RIGHT_TOP:    std::cout << "W"; break;
+            //     case WALL_RIGHT_BOTTOM: std::cout << "W"; break;
+            //     case WALL_SIDE_LR:      std::cout << "W"; break;
+            //     case WALL_SIDE_TB:      std::cout << "W"; break;
+            //     case WALL_END_LEFT:     std::cout << "W"; break;
+            //     case WALL_END_RIGHT:    std::cout << "W"; break;
+            //     case WALL_END_TOP:      std::cout << "W"; break;
+            //     case WALL_END_BOTTOM:   std::cout << "W"; break;
+            //     case WALL_ALL:          std::cout << "W"; break;
+            //     case FLOOR: case AISLE: std::cout << " "; break;
+            //     case STEP:              std::cout << "S"; break;
+            // }
 		}
 		std::cout << std::endl;
 	}
@@ -86,7 +87,7 @@ void out()
 
 int main() {
     areaDivide.initFloor();
-    areaDivide.generate();
+    areaDivide.generate(82);
     areaDivide.fillSurround();
     areaDivide.randomEraseDeadEnd();
     areaDivide.identificationWallKind();
